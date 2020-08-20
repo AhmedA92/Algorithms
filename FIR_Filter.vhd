785afaa2578932e680012bd77880114f37286f0d
Ahmed Moustafa <Developer_msg@hotmail.com>
2020-08-21 00:10:25 +0200

    Add files via upload

--- FIR_Filter.vhd
+++ FIR_Filter.vhd
@@ -19,16 +19,33 @@ entity FIR_Filter is generic(data_width : integer := 16;                        
 end FIR_Filter;
 
 architecture Behavioral of FIR_Filter is
+
 --Filter Coefficients
-type coeficient_type is array (1 to taps) of std_logic_vector(data_width-1 downto 0);  
-constant coeficient: coeficient_type := (X"F1",X"F3",X"07",X"26",X"42",X"4E",X"42",X"26",X"07",X"F3",X"F1");  
-type shift_reg_type is array (0 to taps-1) of std_logic_vector(data_width-1 downto 0);  
+type coeficient_type is array (0 to taps) of signed(data_width-1 downto 0);  
+constant coeficients: coeficient_type := (X"F1",X"F3",X"07",X"26",X"42",X"4E",X"42",X"26",X"07",X"F3",X"F1");  
+
+--shift register for the input data.
+type shift_reg_type is array (0 to taps-1) of signed(data_width-1 downto 0);  
 signal input_shift_reg : shift_reg_type;
-type accum_shift_reg_type is array (0 to taps-1) of std_logic_vector((data_width*2)-1 downto 0);  
+
+--2 shift registers to save and shift values calculated from addition and multiplication.
+type accum_shift_reg_type is array (0 to taps-1) of signed((data_width*2)-1 downto 0);  
 signal mult : accum_shift_reg_type;
 signal add: accum_shift_reg_type;
 
 begin
 
+process (clk) begin
+    if (rst = '1') then
+        D_out <= (others => '0');
+    else 
+        if rising_edge(clk) then
+            input_shift_reg(0) <= D_in;
+            mult(0) <= D_in * coeficients(0);   
+            add(0) <= D_in * coeficients(0);
+        end if;    
+    end if;
+
+end process;
 
 end Behavioral;
