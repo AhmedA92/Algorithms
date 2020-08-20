b89c501f463cd7420d070acdc4186ab728eb09dd
Ahmed Moustafa <Developer_msg@hotmail.com>
2020-08-20 23:49:41 +0200

    Add files via upload

--- FIR_Filter.vhd
+++ FIR_Filter.vhd
@@ -19,6 +19,14 @@ entity FIR_Filter is generic(data_width : integer := 16;                        
 end FIR_Filter;
 
 architecture Behavioral of FIR_Filter is
+--Filter Coefficients
+type coeficient_type is array (1 to taps) of std_logic_vector(data_width-1 downto 0);  
+constant coeficient: coeficient_type := (X"F1",X"F3",X"07",X"26",X"42",X"4E",X"42",X"26",X"07",X"F3",X"F1");  
+type shift_reg_type is array (0 to taps-1) of std_logic_vector(data_width-1 downto 0);  
+signal input_shift_reg : shift_reg_type;
+type accum_shift_reg_type is array (0 to taps-1) of std_logic_vector((data_width*2)-1 downto 0);  
+signal mult : accum_shift_reg_type;
+signal add: accum_shift_reg_type;
 
 begin
 
